8d71cd6e76286ccd7ec3be77159a66579a969ee4
yzwduck <yzwduck@gmail.com>
2023-07-08 10:45:36 +0800

    test texlive fix extract 2

--- .github/workflows/get-tl.sh
+++ .github/workflows/get-tl.sh
@@ -7,6 +7,6 @@ echo "/tmp/some_dir" >> "$GITHUB_PATH"
 
 curl -fLo "$RUNNER_TEMP/texlive.iso" https://mirror.ctan.org/systems/texlive/Images/texlive.iso
 mkdir -p "$RUNNER_TEMP/tl"
-7z e -o "$RUNNER_TEMP/tl" "$RUNNER_TEMP/texlive.iso"
+7z e -o"$RUNNER_TEMP/tl" "$RUNNER_TEMP/texlive.iso"
 rm "$RUNNER_TEMP/texlive.iso"
 ls -al "$RUNNER_TEMP/tl/"
