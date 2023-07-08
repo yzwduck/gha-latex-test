5f54666729d69bed515603f0de409543736605c3
yzwduck <yzwduck@gmail.com>
2023-07-08 11:00:08 +0800

    stream decompress +windows

--- .github/workflows/get-tl.sh
+++ .github/workflows/get-tl.sh
@@ -3,10 +3,14 @@
 set -xe
 
 echo "hello"
+uname
+uname -sr
+uname -a
 echo "/tmp/some_dir" >> "$GITHUB_PATH"
 
-curl -fLo "$RUNNER_TEMP/texlive.iso" https://mirror.ctan.org/systems/texlive/Images/texlive.iso
 mkdir -p "$RUNNER_TEMP/tl"
-7z x -o"$RUNNER_TEMP/tl" "$RUNNER_TEMP/texlive.iso"
-rm "$RUNNER_TEMP/texlive.iso"
+# curl -fLo "$RUNNER_TEMP/texlive.iso" https://mirror.ctan.org/systems/texlive/Images/texlive.iso
+# 7z x -o"$RUNNER_TEMP/tl" "$RUNNER_TEMP/texlive.iso"
+# rm "$RUNNER_TEMP/texlive.iso"
+curl -fL https://mirror.ctan.org/systems/texlive/Images/texlive.iso | bsdtar -xvC "$RUNNER_TEMP/tl"
 ls -al "$RUNNER_TEMP/tl/"
